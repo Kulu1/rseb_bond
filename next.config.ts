21220fb3de1c3975c73be722e8abd198d22e4642
Karma Wangchuk <12220034.gcit@rub.edu.bt>
2025-11-24 21:20:30 +0600

    Deployment3

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,7 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
-   ignoreDuringBuilds: true,
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  eslint: {
+    // ⚠️ This allows production builds to succeed even if there are ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 };
-
-export default nextConfig;
